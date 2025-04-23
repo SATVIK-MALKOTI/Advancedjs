53b08962ef3af25d7b0b9b39861f06407019c8fa
SATVIK-MALKOTI <firebirdfreeze@gmail.com>
2025-04-23 15:24:41 +0530

    4.23.25

---
+++ class.js
@@ -0,0 +1,220 @@
+// let dog = {
+//     dogname:"javascript",
+//     weight: 10,
+//     age: 5,     
+//     color:"brown",
+//     bark:function(){
+//         console.log("woof woof")
+//     }
+// }
+
+// class ClassName{
+//      constructor(prop1,prop2){
+//         this.prop1 = prop1;
+//         this.prop2 = prop2; 
+//     }
+
+// }
+// let obj = new ClassName("arg1","arg2")
+// console.log(obj.prop1)
+// console.log(obj.prop2)
+
+// class Dog{
+//     constructor(dogname, weight, age, color) {
+//         this.dogname = dogname;
+//         this.weight = weight;
+//         this.breed = breed;
+//         this.color = color;
+//     }
+
+//  }
+// let dog1 = new Dog("javascript", 10, 5, "brown")
+// console.log(dog1.dogname);
+// console.log(dog1.weight);
+
+
+//Classes are blueprint for objects
+//CLass names should start by capital letter
+
+// class Person{
+//     constructor(firstname,secondname='doe'){//default arg
+//         this.firstname = firstname;
+//         this.secondname = secondname;
+//     }
+// }
+// let person1 = new Person("John");
+// console.log(person1.firstname);
+// console.log(person1.secondname);
+// console.log(person1);
+
+//functions on a class are called methods
+
+// class Person{
+//     constructor(firstname,secondname='doe'){//default arg must be trailing arguments
+//         this.firstname = firstname;
+//         this.secondname = secondname;
+//     }
+//     greet(){
+//         console.log("hello " + this.firstname + " " + this.secondname);
+//     }
+
+// }
+// let person1 = new Person('john');
+// person1.greet()
+// console.log(person1.firstname);
+// console.log(person1.secondname);
+
+
+                                            //METHODS 
+
+
+//functions on a class are called methods
+//function keyword is not used in a class
+
+// class Person{
+//     constructor(firstname,secondname='doe'){//default arg must be trailing arguments
+//         this.firstname = firstname;
+//         this.secondname = secondname;
+//     }
+//     greet(){
+//         console.log("hello " + this.firstname + " " + this.secondname);
+//     }
+//     compliment(name,object){
+//         console.log("That's wonderful " + this.firstname + " " + this.secondname + " " + name + " " + object);
+//     }
+// }
+// let person = new Person('john','mayer');
+// person.compliment(",whats",'up');
+// person.greet();
+
+
+//PROPERTIES 
+
+//use # for private access
+//cannot be accessed outside the class 
+// class Person{
+//     #firstname;  //private property
+//     #secondname; //private property  
+//     constructor(firstname,secondname){
+//         this.#firstname = firstname;
+//         this.#secondname  = secondname;
+
+//     }
+// }
+
+// let p = new Person('john','doe');
+// console.log(p.firstname); //cannot access a private member
+
+// class Person{
+//     #firstname
+//     #secondname
+//     constructor(firstname,secondname){
+//         if(firstname.startswith('M')){
+//             this.#firstname = firstname;
+//         }
+//         else{
+//             this.#firstname = 'M'+firstname;
+//         }
+//          this.#secondname = secondname;
+//     }
+// }
+
+
+// class Person{
+//     #firstname
+//     #secondname
+//     constructor(firstname,secondname){
+//         this.#firstname  = firstname;
+//         this.#secondname  = secondname;
+
+//     }
+//     get firstname(){
+//         return this.#firstname;
+//     }
+//     set firstname(firstname){
+//         this.#firstname = firstname;
+//     }
+//     get secondname(){
+//         return this.#secondname;
+//     }
+//     set secondname(secondname){
+//         this.#secondname = secondname;
+//     }
+    
+    
+// }
+// let p = new Person('john','doe')
+
+// p.firstname = 'hello'
+// console.log(p.firstname);
+
+
+//GIT
+/* 
+1. git init
+2. git add . 
+3. git commit -m
+4. git push -u
+*/
+
+                                            //INHERITANCE
+                                    
+// class Vehicle{
+//     constructor(color,currentspeed,maxspeed){
+//         this.color = color;
+//         this.currentspeed = currentspeed;
+//         this.maxspeed = maxspeed;
+//     }
+//     move(){
+//         console.log("moving at " + this.currentspeed + "km/h");
+//     }
+//     accelerate(amount){
+//         this.currentspeed += amount;
+//     }
+// }
+
+// class Motorcycle extends Vehicle{
+//     constructor(color,currentspeed,maxspeed,fuel){
+//         super(color,currentspeed,maxspeed); //used to call the constructor of the parent class
+//         this.fuel = fuel;
+//     }
+//     dowheelie(){
+//         console.log("doing a wheelie");
+//     }
+// }
+
+// let motor = new Motorcycle("red", 0, 250, "petrol");
+// //let for object creation 
+// console.log(motor.color); 
+// motor.accelerate(50);
+// console.log(motor.currentspeed); 
+
+                                                //PROTOTYPES
+//mechanism by which objects can inherit features from one another
+class Person{
+    constructor(firstname,secondname){
+        this.firstname = firstname;
+        this.secondname = secondname;
+    }
+    greet(){
+        console.log("HI there");
+    }
+}
+
+//using prototype to add a method to the Person class
+
+Person.prototype.newmethod = function(){
+    console.log('hi ' + this.firstname + " " + this.secondname);
+}
+Person.prototype.favcolor = 'blue'; 
+//adding a property to the Person class
+
+let object = new Person('john','doe');
+object.greet(); //HI there
+object.newmethod(); //hi john doe
+console.log(object.favcolor); //blue
+
+
+
+
+
--- script.js
+++ script.js
@@ -100,5 +100,215 @@
 // }
 // console.log(resArr);
 
-let add2numbers = (x,y) => x+y;
+// let add2numbers = (x,y) => x+y;
+
+//VARIABLE SCOPE AND FUNCTIONS
+
+
+
+// function testavailability(x){
+//     console.log("available here ",x);
+// }
+// testavailability("hi");
+// console.log("available here",x); // x is not defined here 
+
+
+// function testavailability(){
+//     let y = "local variable";
+//     console.log("available here: ",y);
+//  }
+// testavailability("hi");
+// console.log("available here",y); // will not work 
+
+// function testavailability(){
+
+//     let y = "local variable";
+//     console.log("available here: ",y);
+//     return y
+//  }
+// let z = testavailability();
+// console.log("outside the function",z);  
+// console.log("not available here",y); // y is not defined here
+
+
+// function doingstuff(){
+//     if(true){
+//         var x = "local";
+//     }
+//     console.log(x); // x works since var is function scoped
+// }
+// doingstuff();
+
+// function doingstuff(){
+//     if(true){
+//         console.log(x); //REFERENCE ERROR will not work before initialization
+//         let x = "local";
+//     }
+    
+// }
+// doingstuff();
+
+
+
+// function doingstuff(){
+//     if(true){
+//         console.log(x); // will work before initialization
+//         var x = "local";
+//     }
+    
+// }
+// doingstuff();
+
+// function doingstuff(){
+//     if(true){
+//         console.log(x); // will work before initialization
+//         const x = "local";
+//     }
+    
+// }
+// doingstuff();
+
+// let globvar = "global variable";
+// console.log(globvar); // global variable is available here
+
+// function createnewscope(x){
+//     console.log(globvar); // global variable is available here
+    
+// }
+// createnewscope("some parameter");
+// console.log(globvar); // global variable is available here
+
+
+// let x = "global variable";
+
+// function doingstuff(){
+//     let x = "local variable"; // local variabl overrides the global variable
+//     console.log(x); // local variable is available here
+// }
+// doingstuff();
+// console.log(x); // global variable is available here
+
+
+//on removing let , local will be printed twice beacuse value inside the global variable is reassigned 
+
+// let x = "global variable";
+// function doingstuff(x){
+//      console.log(x); // parameter names overrides the global variable
+// }
+// doingstuff("parameter"); 
+
+
+//without let or var keywords variable starts actting as global variable
+// function confuse(){
+//   x = "guess my scope";
+//   console.log('inside the function',x); 
+// }
+// confuse(); // guess my scope
+// console.log('outside the function',x); // guess my scope
+
+//THE immediately involved function expression (IIFE) is a way of expressing a function so that it 
+// gets invoked immeddiately. It is anonymous, it doesnt have a name
+
+// (function(){
+//     console.log("IIFE")
+// })();
+
+// (function(x,y){
+//     console.log(x+y)
+// })(2,3);
  
+// (() =>{
+//     console.log("run right away");
+// })()
+
+
+//RECURSIVE FUNCTIONS:
+//
+// function getRec(nr){
+//     if(nr==0){
+//         return 1;
+//     }
+//     console.log(nr);
+//     getRec(--nr);
+// }
+// getRec(3);
+
+// function logRec(nr){
+//     console.log('started function:',nr);
+//     if(nr>0){
+//         logRec(nr-1);
+//     }
+//     else{
+//         console.log("done with recursion");
+//     }
+// console.log("ended funcntion:",nr)
+// }
+// logRec(3);
+
+
+// function getrec(nr){
+//     console.log(nr);
+//     if(nr>0){
+        
+//         getrec(--nr);
+//     }
+//     console.log(nr);
+   
+// }
+// getrec(3);
+
+// performance of recursion is slower than iteration using loop
+
+//NESTED FUNCTIONS:
+
+// function doOuterFunctionStuff(nr){
+//     console.log("outer function");
+//     doInnerFunctionStuff(nr);
+//     function doInnerFunctionStuff(x){
+//         console.log(x+7);
+     
+//     console.log("I can access outer variables here",nr);
+//                                     }
+// }//cannot access x outside inner function
+// doOuterFunctionStuff(2);
+
+// //anonymous functions
+// let funcVar  = function(){
+//     console.log("anonymous function")
+// };
+// funcVar();
+
+// //FUNCTION CALL BACKS 
+
+// function doStuff(executestuff){
+//     //callback
+//     executestuff();
+//     console.log("inside dostuff functions")
+    
+// }
+// doStuff(funcVar)
+
+//IN JS there are mnay inbuilt functions like settimeout functions, setinterval functions,
+
+// let youGotThis = function(){
+//     console.log("you got this")}
+
+// setTimeout(youGotThis,2000); // 2 seconds delay before executing the function
+
+// setInterval(youGotThis,2000); // 2 seconds delay before executing the function
+
+(function(){
+    console.log("Welcome");
+})();
+(function(){
+ let first = 'laurence';
+ return first;
+})()
+let result = (function(){
+    let first = 'laurence';
+    return first;
+})();
+console.log(result); // laurence
+(function(first){
+    console.log('my name is '+first);
+})('laurence');
